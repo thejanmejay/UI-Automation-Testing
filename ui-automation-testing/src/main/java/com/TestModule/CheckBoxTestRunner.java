5562fab0db6d1718f9181caa11560cc0cee5d9c4
Janmejay Kumar Singh <janmejaysingh331@gmail.com>
2025-08-11 11:25:25 +0530

    CheckBoxTestRunnerPatch Added

--- ui-automation-testing/src/main/java/com/TestModule/CheckBoxTestRunner.java
+++ ui-automation-testing/src/main/java/com/TestModule/CheckBoxTestRunner.java
@@ -1,39 +1,9 @@
 package com.TestModule;
 
-import org.openqa.selenium.WebDriver;
-
 public class CheckBoxTestRunner {
-	WebDriver driver;
 
-	public CheckBoxTestRunner(WebDriver driver) {
-		this.driver = driver;
-
+	public CheckBoxTestRunner() {
+		// TODO Auto-generated constructor stub
 	}
 
-	public void performCheckBoxTest() {
-		if (driver == null) {
-			System.err.println("WebDriver is not initialized.");
-			return;
-		}
-
-		// Use the passed driver, don't create a new one
-		try {
-
-			// Create instance of your logic class
-			CheckBoxActions checkboxActions = new CheckBoxActions(driver);
-
-			// Call high-level test steps
-			checkboxActions.clickCheckboxSection();
-			checkboxActions.clickHomeCheckbox();
-			checkboxActions.expandAll();
-			checkboxActions.selectChildCheckboxes();
-			checkboxActions.expandDocumentAndDownload();
-			checkboxActions.selectGrandchildCheckboxes();
-			checkboxActions.collapseAll();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
 }
