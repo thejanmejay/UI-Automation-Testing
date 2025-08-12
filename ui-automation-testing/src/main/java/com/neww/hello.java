e01a3fcb1060628db7958e9c06a2f1cdd1e25d55
Janmejay Kumar Singh <janmejaysingh331@gmail.com>
2025-08-12 16:04:47 +0530

    com.neww package updated Branch cleanup

--- ui-automation-testing/src/main/java/com/neww/hello.java
+++ ui-automation-testing/src/main/java/com/neww/hello.java
@@ -3,5 +3,13 @@ package com.neww;
 public class hello {
 public static void main(String[] args) {
 	System.out.println("Hello world");
+	
+	System.out.println("Hello world 2");
+	System.out.println("Hello world 2.1");
+	System.out.println("Hello world 3");
+	System.out.println("Hello world 4");
+	System.out.println("Hello world 5");
+	System.out.println("Hello world 6");
+	System.out.println("Hello world 7");
 }
 }
